622c21324f669aa0376c0d79c336d89cf378af36
xrubens-cloud <rubenlambertemail@gmail.com>
2026-04-07 02:11:09 -0400

    updated project

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,11 +3,31 @@ const btnSearch = document.getElementById('btnSearch');
 const destinations = [];
 
 
+
+
 function searchDestination(){
+  
+    const input = document.getElementById('searchKeyword').value.toLowerCase();
 
-    const searchKeyword = document.getElementById("searchKeyword").value;
+   
 
+    fetch('travel_recommendation.json')
+        .then(response => response.json())
+        .then(data => {
+            populateResult(data[input]);
+        })
+        .catch(error => console.error('Error:', error));
 }
 
 
+function populateResult(destination){
+    const resultDiv = document.getElementById('result');
+    console.log(destination);
+}
 
+function clearSearch(){
+    destinations.splice(0,destinations.length);
+}
+
+
+btnSearch.addEventListener('click', searchDestination);
