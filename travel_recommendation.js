382692011f922910422318158503fa461e1d1318
xrubens-cloud <rubenlambertemail@gmail.com>
2026-04-07 00:02:26 -0400

    update again

---
+++ travel_recommendation.js
@@ -0,0 +1,13 @@
+const searchResult = document.getElementById("searchResult");
+const btnSearch = document.getElementById('btnSearch');
+const destinations = [];
+
+
+function searchDestination(){
+
+    const searchKeyword = document.getElementById("searchKeyword").value;
+
+}
+
+
+
