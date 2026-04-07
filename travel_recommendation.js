b5b988316598f69279520d58ca5e4a56ee3cbfe2
xrubens-cloud <rubenlambertemail@gmail.com>
2026-04-07 04:36:08 -0400

    final run

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,10 +1,6 @@
 const searchResult = document.getElementById("searchResult");
 const btnSearch = document.getElementById('btnSearch');
 
-
-
-
-
 function searchDestination(){
   
     const input = document.getElementById('searchKeyword').value.toLowerCase();
@@ -16,13 +12,7 @@ function searchDestination(){
         })
         .then(destinations => {
             console.log(destinations);
-            if(destinations == undefined){
-                noResult();
-            }else{
-          
-                populateResult(destinations);
-             
-            }
+            populateResult(destinations);
     
         })
         .catch(error => console.error('Error:', error));
@@ -30,47 +20,51 @@ function searchDestination(){
 
 
 function populateResult(destinations){
-    const resultDiv = document.getElementById('searchResult');
+    const parentDiv = document.getElementById('content');
+    parentDiv.innerHTML='';
+    const resultDiv = document.createElement('searchResult');
+    resultDiv.className= "searchResult";
     resultDiv.innerHTML = '';
-    destinations.forEach(destination => {
-        if(destination.cities === undefined){
-            const newDiv = document.createElement("div");
-            newDiv.innerHTML += `<img src="${destination.imageUrl}" alt="nopic">`;
-            newDiv.innerHTML += `<h3>${destination.name}</h3>`;
-            newDiv.innerHTML += `<p>${destination.description}</p>`;
-            resultDiv.appendChild(newDiv);
-            console.log(resultDiv);
-        }else if (destination.cities.length>0){
-            destination.cities.forEach(city => {
+
+    if(destinations===undefined){
+        const newDiv = document.createElement("div");
+        newDiv.className ="searchItem"
+        newDiv.innerHTML += `<h3>No result found</h3>`;
+        resultDiv.appendChild(newDiv);
+    }else{
+        destinations.forEach(destination => {
+            if(destination.cities === undefined){
                 const newDiv = document.createElement("div");
-                newDiv.innerHTML += `<div>`;
-                newDiv.innerHTML += `<img src="${city.imageUrl}" alt="nopic">`;
-                newDiv.innerHTML += `<h3>${city.name}</h3>`;
-                newDiv.innerHTML += `<p>${city.description}</p>`;
-          
+                newDiv.className ="searchItem"
+                newDiv.innerHTML += `<img src="${destination.imageUrl}" alt="nopic">`;
+                newDiv.innerHTML += `<h3>${destination.name}</h3>`;
+                newDiv.innerHTML += `<p>${destination.description}</p>`;
                 resultDiv.appendChild(newDiv);
-            })
-        }else{
-            const newDiv = document.createElement("div");
-            resultDiv.newDiv += `<h3>No result found</h3>`;
-            resultDiv.appendChild(newDiv);
-        }
-    })
+                console.log(resultDiv);
+            }else if (destination.cities.length>0){
+                destination.cities.forEach(city => {
+                    const newDiv = document.createElement("div");
+                    newDiv.className ="searchItem"
+                    newDiv.innerHTML += `<div>`;
+                    newDiv.innerHTML += `<img src="${city.imageUrl}" alt="nopic">`;
+                    newDiv.innerHTML += `<h3>${city.name}</h3>`;
+                    newDiv.innerHTML += `<p>${city.description}</p>`;
+              
+                    resultDiv.appendChild(newDiv);
+                })
+            }else{
+                const newDiv = document.createElement("div");
+                newDiv.className ="searchItem"
+                newDiv.innerHTML += `<h3>No result found</h3>`;
+                resultDiv.appendChild(newDiv);
+            }
+    
+    
+            parentDiv.appendChild(resultDiv);
+    
+        })
+    }
 
-   
- //   console.log(destination);
-}
-
-function noResult(){
-    const resultDiv = document.getElementById('searchResult');
-    resultDiv.innerHTML = '';
-    const newDiv = document.createElement("div");
-    resultDiv.newDiv += `<h3>No result found</h3>`;
-    resultDiv.appendChild(newDiv);
-}
-
-function clearSearch(){
-    destinations.splice(0,destinations.length);
 }
 
 
